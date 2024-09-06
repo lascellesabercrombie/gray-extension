56165fc0c79843cd7f1ec544484475ada93a9d65
Alex Grafen <alex.grafen@gmail.com>
2024-09-06 17:05:52 +0100

    feat: initial setup to allow player speech

--- contents/player.tsx
+++ contents/player.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import cssText from "data-text:~/contents/player.css"
 import type { PlasmoCSConfig } from "plasmo"
 
@@ -17,21 +17,52 @@ export const getStyle = () => {
 const PlasmoOverlay = () => {
     const [right, setRight] = useState(10)
     const [bottom, setBottom] = useState(10)
+    const [title, setTitle] = useState(null)
+    const [isMenuVisible, setIsMenuVisible] = useState(false)
+    const [isSpeechBubbleVisible, setIsSpeechBubbleVisible] = useState(false)
       
+useEffect(() => {
+  if (document?.querySelector('h1')) {
+    setTitle(document?.querySelector('h1').innerText)
+  }
+}, [])
+
+
   return (
     <div
-      id="player"
+      id="player-area"
       style={{
+        display: 'flex',
         padding: 12,
         right,
         bottom: bottom
       }}
       >
-        <button onClick={(e) => {setRight(right + 10)}}>Left</button>
-        <button onClick={(e) => {setRight(right - 10)}}>Right</button>
-        <button onClick={(e) => {setBottom(bottom + 10)}}>Up</button>
-        <button onClick={(e) => {setBottom(bottom - 10)}}>Down</button>
+        {isMenuVisible && <div>
+          <button onClick={() => {
+          setIsSpeechBubbleVisible(!isSpeechBubbleVisible)
+          setIsMenuVisible(!isMenuVisible)
+          }}>Speak</button></div>}
+        {isSpeechBubbleVisible && <span>{`Ahhh, ${title}!`}</span>}
+        <div   style={{
+        display: 'flex',
+        flexDirection: 'column'
+        }}>
+         <button onClick={() => {setBottom(bottom + 10)}}>Up</button>
+         <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <button onClick={() => {setRight(right + 10)}}>Left</button>
+          <span>Player</span>
+        <button onClick={() => {
+          setIsMenuVisible(!isMenuVisible)
+          setIsSpeechBubbleVisible(false)
+          }}>See menu</button>
+        <button onClick={() => {setRight(right - 10)}}>Right</button>
+        </div>
+        <button onClick={() => {setBottom(bottom - 10)}}>Down</button>
+        </div>
+        
     </div>
+
   )
 }
 
