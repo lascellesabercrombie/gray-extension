9ccc6ab0f1a24c3be773531a515c48a8bf9dc841
Alex Grafen <alex.grafen@gmail.com>
2024-09-30 15:20:17 +0100

    feat: some styling of popup

--- popup.tsx
+++ popup.tsx
@@ -1,17 +1,28 @@
 import { sendToBackground } from "@plasmohq/messaging"
-import { useState } from "react"
+import "contents/player.css"
 
 function IndexPopup() {
 
   return (
     <div
       style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to the Gray Extension
-        </h2>
-        <button onClick={async() => { await sendToBackground({
+        border: "0.4rem double black",
+        display: "flex",
+        fontFamily: 'serif',
+        flexDirection: "column",
+        padding: "15px 25px",
+        textAlign: "center",
+        minWidth: "100px",
+        alignItems: "center"
+      }}
+      >
+      <h1 style={{textTransform: 'uppercase'}}>
+        Poor Codings
+        </h1>
+        <h2 style={{fontStyle: 'italic'}}>An unofficial homage to the work of Alasdair Gray</h2>
+        <button 
+        className="speech-bubble-button"
+        onClick={async() => { await sendToBackground({
   name: "start",
   body: {
     showPlayer: true
